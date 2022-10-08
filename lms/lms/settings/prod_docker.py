28c20bdab423f8f08d3b513c43b60ed6f36ca43a
bd240897 <bd240897@yandex.ru>
2022-10-09 00:32:39 +0300

    docker v4

---
+++ lms/lms/settings/prod_docker.py
@@ -0,0 +1,22 @@
+from .base import *
+from os import environ as os_environ
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-%gmzr)1n3^6m=0w^0y@^5mewmjb4yn*b#j6#-(f=6uy4)dg&+r'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['127.0.0.1', 'bad-philosopher.ru', 'www.bad-philosopher.ru', "45.67.58.152"]
+
+# Database DOCKER
+DATABASES = {
+    'default': {
+        'ENGINE': os_environ.get('POSTGRES_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os_environ.get('POSTGRES_DB', str(BASE_DIR.path('db.sqlite3')),),
+        'USER': os_environ.get('POSTGRES_USER', 'user'),
+        'PASSWORD': os_environ.get('POSTGRES_PASSWORD', 'password'),
+        'HOST': os_environ.get('POSTGRES_HOST', 'localhost'),
+        'PORT': os_environ.get('POSTGRES_PORT', '5432'),
+    }
+}
--- lms/lms/wsgi.prod.py
+++ lms/lms/wsgi.prod.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lms.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lms.settings.prod')
 
 application = get_wsgi_application()
