150fa7307cf9cbdae43cfb268b883d6bbd69ee9e
bd240897 <bd240897@yandex.ru>
2022-10-09 11:02:50 +0300

    fixed edit profile and admin panel

--- lms/bank/forms.py
+++ lms/bank/forms.py
@@ -23,16 +23,6 @@ class RegisterUserForm(UserCreationForm):
     password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs=param_password1))
     password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs=param_password2))
 
-    # class Meta:
-    #     model = User
-    #     fields = ('username', 'email', 'password1', 'password2')
-    #     widgets = {
-    #         'username': forms.TextInput(attrs={'class': 'form-input'}),
-    #         'password1': forms.PasswordInput(attrs={'class': 'form-input'}),
-    #         'password2': forms.PasswordInput(attrs={'class': 'form-input'}),
-    #         'email': forms.EmailInput(attrs={'class': 'form-input'}),
-    #     }
-
 
 class LoginUserForm(AuthenticationForm):
     """Форма логина"""
@@ -47,22 +37,6 @@ class LoginUserForm(AuthenticationForm):
 
     username = forms.CharField(label='Логин', widget=forms.TextInput(attrs=parm_username))
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs=param_password))
-
-    # class Meta:
-    #     parm_username = {"type": "text",
-    #                      "class": "form-control",
-    #                      "placeholder": "Напишите Ваше сообщение", }
-    #
-    #     param_password = {"type": "text",
-    #                       "class": "form-control",
-    #                       "placeholder": "Напишите номер раунда", }
-    #
-    #     fields = ('username', 'password')
-    #
-    #     widgets = {
-    #         'username': forms.TextInput(attrs=parm_username),
-    #         'password': forms.PasswordInput(attrs=param_password),
-    #     }
 
 
 class TransferCoinForm(forms.Form):
@@ -83,8 +57,7 @@ class TransferCoinForm(forms.Form):
                       "placeholder": "Количество", }
 
     type_coin = forms.ChoiceField(widget=forms.Select(attrs=type_coin), choices=CHOICES, initial='matic')
-    to_account = forms.CharField(label='', widget=forms.TextInput(attrs=to_account_attr),
-                                 initial='0x15Cc4abzz27647ec9fE70D892E55586074263dF0')
+    to_account = forms.CharField(label='', widget=forms.TextInput(attrs=to_account_attr),) # initial='0x15Cc4abzz27647ec9fE70D892E55586074263dF0'
     amount = forms.CharField(label='', widget=forms.TextInput(attrs=amount_id_attr), initial='1')
 
 
@@ -98,9 +71,8 @@ class TransferNFTForm(forms.Form):
                      "class": "form-control",
                      "placeholder": "Номер NFT", }
 
-    to_account = forms.CharField(label='', widget=forms.TextInput(attrs=to_account_attr),
-                                 initial='0x15Cc4abzz27647ec9fE70D892E55586074263dF0')
-    token_id = forms.CharField(label='', widget=forms.TextInput(attrs=token_id_attr), initial='5')
+    to_account = forms.CharField(label='', widget=forms.TextInput(attrs=to_account_attr)) # initial='0x15Cc4abzz27647ec9fE70D892E55586074263dF0'
+    token_id = forms.CharField(label='', widget=forms.TextInput(attrs=token_id_attr)) # , initial='5'
 
 
 class GenerateNFTForm(forms.Form):
@@ -151,7 +123,7 @@ from django.contrib.auth.models import User
 
 class CreateGroupForm(forms.Form):
     name = forms.CharField()
-    owner = forms.ChoiceField(choices=[])
+    owner = forms.ChoiceField(choices=[]) # , widget=forms.Select(attrs={"class": "form-select"})
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
