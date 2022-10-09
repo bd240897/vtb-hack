f3cfd0425dfb947458a5a3f95d27db9cc07e1041
bd240897 <bd240897@yandex.ru>
2022-10-09 20:09:07 +0300

    test2

--- lms/lms/urls.py
+++ lms/lms/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     # path('polls/', include('polls.urls')),
 ]
 
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [path('__debug__/', include(debug_toolbar.urls)),] + urlpatterns
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     import debug_toolbar
+#     urlpatterns = [path('__debug__/', include(debug_toolbar.urls)),] + urlpatterns
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
